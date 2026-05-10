9058356d6b1441c1b2f61de80c37148ba5ccf19d
Jarno Saarinen <jarnosaarinen@subsoccer.com>
2026-05-10 19:49:08 +0300

    fix: use 1024x1024 size (smallest supported by gpt-image-1)

--- netlify/functions/generate-avatar.js
+++ netlify/functions/generate-avatar.js
@@ -60,9 +60,9 @@ Style: Bold outlines, vibrant cel-shaded comic art, flat colors. Cool street foo
             `--${boundary}\r\nContent-Disposition: form-data; name="model"\r\n\r\ngpt-image-1\r\n`, "utf-8"
         ));
 
-        // -- size (512x512 for much faster generation)
+        // -- size (1024x1024 is the smallest supported by gpt-image-1)
         formParts.push(Buffer.from(
-            `--${boundary}\r\nContent-Disposition: form-data; name="size"\r\n\r\n512x512\r\n`, "utf-8"
+            `--${boundary}\r\nContent-Disposition: form-data; name="size"\r\n\r\n1024x1024\r\n`, "utf-8"
         ));
 
         // -- closing
